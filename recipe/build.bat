57591c6443bc28cc96b72cd2656a4d9ef5b7b0cd
Michał Górny <mgorny@quansight.com>
2025-11-24 17:18:09 +0100

    Add a sanity check for exported symbols on win-64
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,2 +1,7 @@
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if errorlevel 1 exit 1
+
+dumpbin /exports %LIBRARY_BIN%\libblis.4.dll > exports.txt
+type exports.txt
+grep -q cdotc_ exports.txt
+if errorlevel 1 exit 1
